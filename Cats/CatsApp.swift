858bdabeb4c050185c0719712711a3f9062adea5
Alyssa Aw Yong <alyssa.awyong@gmail.com>
2021-09-04 09:25:19 +0800

    Initial Commit

---
+++ Cats/CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsApp.swift
+//  Cats
+//
+//  Created by rgs on 4/9/21.
+//
+
+import SwiftUI
+
+@main
+struct CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
